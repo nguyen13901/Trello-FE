6b6da7ffa2e28f0953d29ca8bb8a49ee562ae248
Pham Thanh Nguyen <nguyen.phamthanh@paradox.ai>
2024-02-15 13:54:41 +0700

    Setup light/dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr/>
       <div>Hello world</div>
       <Button variant="contained">Hello world</Button>
     </>
