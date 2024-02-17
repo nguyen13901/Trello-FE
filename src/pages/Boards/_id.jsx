7f083aa54f8be6b0ede3e96337719abcf7e4d0b3
PhamThanhNguyen <nguyen13901@gmail.com>
2024-02-17 16:12:40 +0700

    Restructure project

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,22 @@
+
+import Container from '@mui/material/Container'
+import AppBar from '../../components/AppBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+
+
+function Board() {
+  return (
+    <Container disableGutters maxWidth
+      sx={{
+        height: '100vh'
+      }}
+    >
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container >
+  )
+}
+
+export default Board
