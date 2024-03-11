73af95fb381e93758da35e6d19dd6c409f2ae309
PhamThanhNguyen <nguyen13901@gmail.com>
2024-03-11 22:19:39 +0700

    Implement drag and drop column

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,22 +1,66 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
 import { mapOrder } from '~/utils/sorts'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 
 function BoardContent({ board }) {
 
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const mouseSensor = useSensor(MouseSensor, {
+    // Require the mouse to move by 10 pixels before activating
+    activationConstraint: {
+      distance: 10
+    }
+  })
+
+  const touchSensor = useSensor(TouchSensor, {
+    // Press delay of 250ms, with tolerance of 5px of movement
+    activationConstraint: {
+      delay: 250,
+      tolerance: 5
+    }
+  })
+
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState()
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+
+    if (!over) return
+
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+      setOrderedColumns(arrayMove(orderedColumns, oldIndex, newIndex))
+    }
+  }
 
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) => theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
-        width: '100%',
-        height: (theme) => theme.trello.boardContentHeight,
-        p: '10px 0px'
-      }}
-    >
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) => theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
+          width: '100%',
+          height: (theme) => theme.trello.boardContentHeight,
+          p: '10px 0px'
+        }}
+      >
+        {orderedColumns && <ListColumns columns={orderedColumns} />}
+      </Box>
+    </DndContext>
   )
 }
 
